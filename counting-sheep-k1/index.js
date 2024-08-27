7d5596eb7bec3017111109ff508c917a0282e8e5
Umka-dev <design.tvin@gmail.com>
2024-08-27 21:03:16 +0200

    add solutions

--- counting-sheep-k1/index.js
+++ counting-sheep-k1/index.js
@@ -12,6 +12,40 @@
 
 // Hint: Don't forget to check for bad values like null/undefined
 
-function countSheeps(sheep) {
-  // TODO
+//My solution
+// function countSheeps(sheeps) {
+//   sheeps = [undefined, null, false, true, true, false, null, undefined];
+
+//   const availableSheepArray = sheeps.filter(function (isSheep) {
+//     return isSheep;
+//   });
+
+//   let count = availableSheepArray.length;
+
+//   console.log('Flock with available sheeps: ', availableSheepArray);
+//   console.log('Number of sheeps in a flock: ', count);
+
+//   return count;
+// }
+
+//Better solution
+// function countSheeps(arrayOfSheeps) {
+//   arrayOfSheeps = [undefined, null, false, true, true, false, null, undefined];
+//   var count = 0;
+
+//   arrayOfSheeps.forEach(function (sheep) {
+//     if (sheep) count++;
+//   });
+//   console.log('Number of sheeps in a flock: ', count);
+//   return count;
+// }
+
+//The Best solution
+function countSheeps(arrayOfSheeps) {
+  arrayOfSheeps = [undefined, null, false, true, true, false, null, undefined];
+  let count = arrayOfSheeps.filter(Boolean).length; //The same as: arrayOfSheeps.filter(function(x){return Boolean(x)}).length;
+  console.log('Number of sheeps in a flock: ', count);
+  return count;
 }
+
+countSheeps();
