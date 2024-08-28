435f2bd65f41329188843d2f18d2062b2ab3e484
Umka-dev <design.tvin@gmail.com>
2024-08-28 12:24:51 +0200

    add solutions

--- convert-string-to-array-k4/index.js
+++ convert-string-to-array-k4/index.js
@@ -5,6 +5,33 @@
 // "I love arrays they are my favorite" ==> ["I", "love", "arrays", "they", "are", "my", "favorite"]
 
 //My solution
-function stringToArray(string) {
-  // code code code
-}
+// function stringToArray(str) {
+//   const strArray = str.split(' ');
+//   console.log('String array: ', strArray);
+//   console.log('Data type is: ', typeof strArray);
+//   console.log('Is array: ', Array.isArray(strArray));
+//   return strArray;
+// }
+
+// stringToArray('I love arrays they are   my        favorite');
+
+//Other solution
+// stringToArray=(s)=>s.split(' ')
+
+//Other solution
+// stringToArray = function (str) {
+//   return str.trim().split(' ');
+// };
+// console.log(stringToArray('  I am learning JavaScript right now  '));
+
+//Better solution
+stringToArray = (str) => {
+  const strArray = str
+    .trim() //delete the first and last spaces
+    .replace(/[!,_-]/g, '') //replace (!,_-) symbols to ("") globally
+    .split(/\s+/g); // supports multiple spaces and tabs
+  console.log('String array: ', strArray);
+  console.log('Data type is: ', typeof strArray);
+  console.log('Is array: ', Array.isArray(strArray));
+};
+stringToArray(' I love arrays, they are   - my        _favorite!  ');
