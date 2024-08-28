cf39c9cb744af7559f1601da379b06e97966caf2
Umka-dev <design.tvin@gmail.com>
2024-08-28 15:27:44 +0200

    first commit

---
+++ reversed-strings-k6/index.js
@@ -0,0 +1,10 @@
+// Task
+// Complete the solution so that it reverses the string passed into it.
+// Examples:
+// 'world'  =>  'dlrow'
+// 'word'   =>  'drow'
+
+//My solution
+function solution(str) {
+  //
+}
