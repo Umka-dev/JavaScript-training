768cf2eb8cfc03a5e78e3797a9b858af4640a618
Umka-dev <design.tvin@gmail.com>
2024-08-28 15:49:26 +0200

    add solutions

--- reversed-strings-k6/index.js
+++ reversed-strings-k6/index.js
@@ -5,6 +5,35 @@
 // 'word'   =>  'drow'
 
 //My solution
-function solution(str) {
-  //
+// function reverseString(str) {
+//   const reverseString = str.split('').reverse().join('');
+//   console.log('Reversed string: ', reverseString);
+//   return reverseString;
+// }
+// reverseString('Testing for fixed tests');
+
+//Explanation
+function reverseString(str) {
+  // Step 1. Use the split() method to return a new array
+  var splitString = str.split(''); // var splitString = "hello".split("");
+  // ["h", "e", "l", "l", "o"]
+
+  // Step 2. Use the reverse() method to reverse the new created array
+  var reverseArray = splitString.reverse(); // var reverseArray = ["h", "e", "l", "l", "o"].reverse();
+  // ["o", "l", "l", "e", "h"]
+
+  // Step 3. Use the join() method to join all elements of the array into a string
+  var joinArray = reverseArray.join(''); // var joinArray = ["o", "l", "l", "e", "h"].join("");
+  // "olleh"
+
+  //Step 4. Return the reversed string
+  console.log('Reversed string: ', joinArray);
+  return joinArray; // "olleh"
 }
+
+reverseString('hello');
+
+//Other solution
+// function solution(str){
+//     return [...str].reverse().join('');
+//   }
