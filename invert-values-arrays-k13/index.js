42f4a69dfa148d5185a59e1635f76e271d210167
Umka-dev <design.tvin@gmail.com>
2024-08-29 18:03:41 +0200

    add solutions

--- invert-values-arrays-k13/index.js
+++ invert-values-arrays-k13/index.js
@@ -7,7 +7,37 @@
 
 // You can assume that all values are integers. Do not mutate the input array.
 
-//
+//My solution
+// function invert(array) {
+//   const invArray = array.map((n) => 0 - n);
+//   console.log('Invetred array: ', invArray);
+//   return invArray;
+// }
+
+//Other solution
 function invert(array) {
-  return;
+  const invArray = array.map((num) => -num);
+  console.log('Invetred array: ', invArray);
+  return invArray;
 }
+
+//Other solution
+// function invert(array) {
+//   const invArray = array.map((x) => (x === 0 ? x : -x));
+//   console.log('Invetred array: ', invArray);
+//   return invArray;
+// }
+
+//Other solution
+// function invert(array) {
+//     return array.map(e=> e*-1);
+//  }
+
+//Other solution
+// function invert(array) {
+//     return array.map(n => n && -n)
+//  }
+
+invert([1, 2, 3, 4, 5]);
+// invert([1, -2, 3, -4, 5]);
+// invert([0]);
