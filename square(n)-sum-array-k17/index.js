6189f2a197e39e6ad048125f46b3f694a34476e4
Umka-dev <design.tvin@gmail.com>
2024-08-30 17:12:57 +0200

    first commit

---
+++ square(n)-sum-array-k17/index.js
@@ -0,0 +1,7 @@
+//Task:
+// Complete the square sum function so that it squares each number passed into it and then sums the results together.
+
+// For example, for [1, 2, 2] it should return 9 because square(1)+square(2)+square(2)=9.
+
+//My solution
+function squareSum(numbers) {}
