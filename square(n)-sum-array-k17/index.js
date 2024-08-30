2cbfb099bc4e1e52159f70dc3fd18f225e577347
Umka-dev <design.tvin@gmail.com>
2024-08-30 17:48:42 +0200

    add solutions

--- square(n)-sum-array-k17/index.js
+++ square(n)-sum-array-k17/index.js
@@ -4,4 +4,28 @@
 // For example, for [1, 2, 2] it should return 9 because square(1)+square(2)+square(2)=9.
 
 //My solution
-function squareSum(numbers) {}
+// function squareSum(numbers) {
+//   const sumSquereNumbers = numbers
+//     .map((number) => number ** 2)
+//     .reduce((acc, number) => acc + number, 0);
+//   console.log('Sum of square of the numbers in the array: ', sumSquereNumbers);
+//   return sumSquereNumbers;
+// }
+
+//Other solution
+// function squareSum(numbers) {
+//   return console.log(
+//     numbers.reduce(function (sum, n) {
+//       return n * n + sum;
+//     }, 0)
+//   );
+// }
+
+//Better solution
+function squareSum(numbers) {
+  const sumSquereNumbers = numbers.reduce((sum, num) => sum + num ** 2, 0);
+  console.log('Sum of square of the numbers in the array: ', sumSquereNumbers);
+  return sumSquereNumbers;
+}
+
+squareSum([0, 3, 4, 5]);
