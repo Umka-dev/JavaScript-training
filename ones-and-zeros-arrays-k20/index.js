05175e8d8557ec6581edc43dea8c0152eb8fa192
Umka-dev <design.tvin@gmail.com>
2024-09-16 17:59:46 +0200

    add other solutions

--- ones-and-zeros-arrays-k20/index.js
+++ ones-and-zeros-arrays-k20/index.js
@@ -15,21 +15,35 @@
 // However, the arrays can have varying lengths, not just limited to 4.
 
 //My solution
+// const binaryArrayToNumber = (arr) => {
+//   arr = [1, 1, 1, 1];
+
+//   function changeElement(x, index, array) {
+//     const n = array.length - 1 - index;
+//     const newX = x * 2 ** n;
+//     console.log('x:', x, 'n:', n, 'newX:', newX);
+//     return newX;
+//   }
+
+//   const newArr = arr.map(changeElement);
+//   const num = newArr.reduce((a, b) => a + b, 0);
+//   console.log('New array: ', newArr);
+//   console.log('Number: ', num);
+//   return num;
+// };
+
+// binaryArrayToNumber();
+
+//Better solution
+
 const binaryArrayToNumber = (arr) => {
   arr = [1, 1, 1, 1];
-
-  function changeElement(x, index, array) {
-    const n = array.length - 1 - index;
-    const newX = x * 2 ** n;
-    console.log('x:', x, 'n:', n, 'newX:', newX);
-    return newX;
-  }
-
-  const newArr = arr.map(changeElement);
-  const num = newArr.reduce((a, b) => a + b, 0);
-  console.log('New array: ', newArr);
-  console.log('Number: ', num);
-  return num;
+  const num = parseInt(arr.join(''), 2); //or parseInt(Number(arr.join('')), 2)
+  console.log('Integer number from the binary array: ', num);
 };
-
 binaryArrayToNumber();
+
+//Other solution
+// function binaryArrayToNumber(arr) {
+//   return arr.reduce( (a, b) => a << 1 | b );
+// }
